4fe4060c833827fa7500a93d532f94b0efd275c7
elijahzimmerman32 <96357379+elijahzimmerman32@users.noreply.github.com>
2022-12-13 01:08:59 -0500

    Add files via upload

--- express.js
+++ express.js
@@ -7,6 +7,10 @@ const fs = require("fs");
 const path = require('path');
 const https = require('https');
 
+//to run local tunnel
+//Set-ExecutionPolicy -ExecutionPolicy Unrestricted -Scope Process
+//lt --port 3000
+
 //handle requests
 const req = require('express/lib/request');
 
@@ -22,7 +26,7 @@ const sqlite3 = require('sqlite3').verbose();
 const session = require('express-session');
 const { time } = require('console');
 const { json } = require('express');
-const config = {MaxLoginTries: 5, AccountLockout: 20*60*1000, SessionTimeOut: 5*60*1000}
+const config = {MaxLoginTries: 5, AccountLockout: 20*60*1000, SessionTimeOut: 10*60*1000}
 
 app.use(session({
     secret: 'keyUSEDtoHASHsessionId',  
@@ -45,7 +49,7 @@ function ClearSession( req ) {
     req.session.isLoggedIn   = false;
     req.session.isAdmin      = false;
     req.session.user         = '';
-    req.session.lastRequest = '2000-01-01T00:00:00.000Z';
+    req.session.lastRequest = '2000-01-01 00:00:00';
 }
 
 function SendInvalidLogin(res, user, msg)
@@ -66,6 +70,76 @@ function SendInvalidLogin(res, user, msg)
     });
 }
 
+function SendInvalidRegister(res, user, msg)
+{
+    // only show message while debugging
+    //msg = 'Invalid login';
+
+    fs.readFile(path.join(__dirname, 'webroot', 'register.html'), 'utf8', function(err, data) {
+        if (err) {
+            res.sendStatus(404);
+        } else {
+  
+            data = data.replace( "const user = '';", `const user = '${user}';`);
+            data = data.replace( "const statusMessage = '';", `const statusMessage = '${msg}';`);
+  
+            res.send(data);
+        }
+    });
+}
+
+function isValidSessionRequest( req, res, reqAdmin, redirect ) {
+    let validated = false;
+
+    let currentTime = new Date();
+
+    //console.log( 'isValidSessionRequest | ', req.session );
+
+    if (req.session && req.session.isLoggedIn && ((req.session.isAdmin == 1) || ! reqAdmin))
+    {
+        let sessionLastRequest = Date.parse(req.session.lastRequest)
+
+        validated = (currentTime - sessionLastRequest <= config.SessionTimeOut);
+    } 
+
+    if (! validated)
+    {   
+        if (redirect)
+            res.redirect( "/login.html" )
+        else
+            res.sendStatus(401);  // Unauthorized
+        return false;
+    }
+    else {
+        req.session.lastRequest = currentTime
+        return true;
+    }
+}
+
+let results = {};
+
+function getData() {
+    let db = new sqlite3.Database('usercount.sqlite');
+    let sql = 'SELECT user, lastLoginAttempt, count FROM Counts'
+    db.all(sql, (err, rows) => {
+        if (err) {
+        return console.error(err.message);
+        }
+        else {
+            rows.forEach((row) => {
+                results[row.user] = {user: row.user, count: row.count,  LastSeen: row.lastLoginAttempt}
+                
+            });
+            return results, console.log(results), db.close()
+        }
+    });
+};
+
+getData();
+let interval = setInterval(getData, 10*1000);
+
+
+
 //Routing
 app.use( (req, res, next) => {
     console.log( req.url );
@@ -84,6 +158,14 @@ app.post('/login.html', (req, res) => {
     let password = req.body.password;
     console.log(user, password)
 
+
+    ///debug the username/password
+    if (! user || ! password)
+    {
+        SendInvalidLogin(res, user, "Missing username or password");
+        return
+    }
+
     ///// connecting to database
     let db = new sqlite3.Database('usercount.sqlite', (err) => {
         if (err) {
@@ -93,7 +175,7 @@ app.post('/login.html', (req, res) => {
         });
 
     /// Time of login request
-    currentLoginTime = 'UPDATE datetime_text SET d = datetime("now") WHERE timenow = 0'
+    currentLoginTime = 'UPDATE datetime_text SET d = datetime("now","localtime") WHERE timenow = 0'
     db.run(currentLoginTime, function(err) {
         if (err) {
             return console.error(err.message);
@@ -112,12 +194,7 @@ app.post('/login.html', (req, res) => {
             
             //console.log(row.user, row.password)
     
-        ///debug the username/password
-        if (! user || ! password)
-        {
-            SendInvalidLogin(res, user, "Missing username or password");
-            return
-        }
+        ///No Username with this title
         if (userInfo == test)
             {
                 SendInvalidLogin(res, user, 'Invalid username');
@@ -156,6 +233,7 @@ app.post('/login.html', (req, res) => {
             {
                 userInfo.badcount++;
                 SendInvalidLogin(res, user, 'Invalid Password');
+                return
             }
             else
             {
@@ -164,16 +242,6 @@ app.post('/login.html', (req, res) => {
                 req.session.user        = user;
                 req.session.lastRequest = currentTimeSQL;
 
-                //need to update counter with correct account
-            // fs.readFile(path.join(__dirname, 'webroot', 'counter.html'), 'utf8', function(err, data) {
-            //     if (err) {
-            //         res.sendStatus(404);
-            //     } else {
-            
-            //         data = data.replace( "const user = '';", `const user = '${user}';`);
-            //         res.send(data);
-            //     }
-            // })
             if (userInfo.isAdmin == 1)
             {
                 qs = (userInfo.isAdmin == 1) ? '?admin=1' : '';
@@ -186,10 +254,8 @@ app.post('/login.html', (req, res) => {
             res.redirect(`/counter.html${qs}`)
             }
 
-            
-
             // updating the database with badcount and last login attempt
-            let sql2 = `UPDATE Counts SET badcount = ${userInfo.badcount}, lastLoginAttempt = datetime("now") WHERE user = ?`;
+            let sql2 = `UPDATE Counts SET badcount = ${userInfo.badcount}, lastLoginAttempt = datetime("now","localtime") WHERE user = ?`;
             db.run(sql2, [user], function(err) {
                 if (err) {
                     return console.error(err.message);
@@ -208,14 +274,106 @@ app.post('/login.html', (req, res) => {
         }
         //console.log('Closed the database connection.');
     });
+});
+
+app.get("/register", (req, res) => {
+    ClearSession(req)
+
+    res.redirect("/register.html")
+  });
+
+app.post("/register.html", (req, res) => {
+    ClearSession(req)
+    let registery = req.body
+    let user = registery.user
+    password = registery.password
+
+    if (! registery.user || ! registery.password || ! registery.confirmpassword)
+    {
+        SendInvalidRegister(res, user, 'Missing Username or Passwords')
+        return
+    }
+
+    if (registery.confirmpassword != registery.password)
+    {
+        SendInvalidRegister(res, user, 'Passwords do not match')
+        return
+    }
+
+    if (registery.user == registery.password)
+    {
+        SendInvalidRegister(res, user, 'Username and Password Cannot Be the Same')
+        return
+    }
+
+    let db = new sqlite3.Database('usercount.sqlite', (err) => {
+        if (err) {
+          return console.error(err.message);
+        }
+        console.log('Connected to the usercount.sqlite database.');
+    });
+
+    let sql = `SELECT user FROM Counts`;
+    let users = [];
+    db.all(sql, (err, rows) => {
+        if (err) {
+        return console.error(err.message);
+        }
+        else {
+            rows.forEach((row) => {
+                //console.log(row.user);
+                users.push(row.user);
+            });
+
+            if (users.includes(user)) {
+                SendInvalidRegister(res, user, 'Username Taken')
+                return
+            }
+            else {
+                let sql2 = `INSERT INTO Counts (user, password, count, isAdmin, lastLoginAttempt, badcount) VALUES (?,?,0,0,datetime("now","localtime"),0)`;
+                db.run(sql2, [user, password], function(err) {
+                    if (err) {
+                        return console.error(err.message);
+                    }
+                    console.log(`Row(s) updated: ${this.changes}`);
+                });   
+            }
+        }   
+    });
+
+    db.close((err) => {
+        if (err) {
+        return console.error(err.message);
+        }
+        console.log('Closed the database connection.');
+    });
+    res.redirect("/login.html")
+});
 
 
-    
+app.get('/logout.html', (req, res) => {
+    ClearSession(req);
+
+    res.redirect("/login.html")
+  });
+
+app.get('/admin', (req, res) => {
+    if (isValidSessionRequest(req, res, true, true))
+        res.redirect("/admin.html")
+        //res.sendFile(path.join(__dirname, 'webroot', 'admin.html'));
+  });
+
+app.get('/counter.html', (req, res) => {
+    if (! isValidSessionRequest(req, res, false, true))
+        return;
+    else {
+        res.sendFile(path.join(__dirname, 'webroot', 'counter.html'));
+    }
 });
 
 app.post('/counter.html', (req, res) => {
-    let dict = req.body
     
+    let dict = req.body
     console.log(dict.user)
 
     //Opening Connection
@@ -223,7 +381,7 @@ app.post('/counter.html', (req, res) => {
     if (err) {
       return console.error(err.message);
     }
-    console.log('Connected to the usercount.sqlite database.');
+    //console.log('Connected to the usercount.sqlite database.');
     });
 
     let sql = `SELECT * FROM Counts WHERE user = ?`;
@@ -233,23 +391,26 @@ app.post('/counter.html', (req, res) => {
           return console.error(err.message);
         }
         else {
-            console.log(row.count, row.user)
+            //console.log(row.count, row.user)
             res.json(row.count)
         }
     });
-    
 
     //closing Database connection when done
     db.close((err) => {
         if (err) {
         return console.error(err.message);
         }
-        console.log('Closed the database connection.');
+        //console.log('Closed the database connection.');
     });
     
 });
 
 app.post('/count', (req, res) => {
+    if (! isValidSessionRequest(req, res, false, true)){
+       return;}
+
+
     let order = req.body
     console.log(order)
 
@@ -258,7 +419,7 @@ app.post('/count', (req, res) => {
         if (err) {
           return console.error(err.message);
         }
-        console.log('Connected to the usercount.sqlite database.');
+        //console.log('Connected to the usercount.sqlite database.');
         });
     
     let sql = `UPDATE Counts SET count=count+${parseInt(order.command)} WHERE user = ?`;
@@ -291,14 +452,48 @@ app.post('/count', (req, res) => {
             if (err) {
             return console.error(err.message);
             }
-            console.log('Closed the database connection.');
+            //console.log('Closed the database connection.');
     });
+});
+
+app.delete('/deleteuser/:user', (req, res) => {
+    if (! isValidSessionRequest(req, res, true, false))
+        return;
+
+    let user = req.params.user;
+    
+    if (user == "admin"){
+        res.sendStatus(405);  // Cannot Delete Admin
+        return
+    }
+
+    let db = new sqlite3.Database('usercount.sqlite')
+    sql = 'DELETE FROM Counts WHERE user = ?'
+    db.run(sql, [user], function(err) {
+        if (err) {
+            return console.error(err.message);
+        }
+        console.log(`Row(s) updated: ${this.changes}`);
+        res.status(200); //
+        res.json({User: user})
+        db.close()
+    });
+    
+
 });
 
 app.get('/logout.html', (req, res) => {
     ClearSession(req);
 
     res.redirect("/login.html")
+});
+
+app.get('/users', (req, res) => {
+    if (! isValidSessionRequest(req, res, true, false))
+        return;
+    
+
+    res.json( results );
 });
 
 app.use(express.static('webRoot'));
